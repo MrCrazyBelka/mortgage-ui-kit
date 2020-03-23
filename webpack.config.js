d4240070078950d87eecce411e0e4ff0bc76748e
MrCrazyBelka <starpatrik06@gmail.com>
2020-03-24 00:23:00 +0300

    exclude node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         }],
       },
       { test: /\.(tsx|ts)$/, loader: "babel-loader", exclude: /node_modules/, },
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader", exclude: /node_modules/ },
       {
         test: /\.(ico|jpg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
         loader: "url-loader",
