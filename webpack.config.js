e68b00572ab22e0e97e516c9fa61bd77a09030a8
MrCrazyBelka <33965940+MrCrazyBelka@users.noreply.github.com>
2020-03-23 15:49:14 +0300

    Webpack TS

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,44 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/lib/index.js'),
+  entry: path.resolve(__dirname, './src/index.ts'),
   output: {
-    path: path.resolve(__dirname, './dist/lib'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'index.js',
     library: '',
     libraryTarget: 'commonjs'
   },
   externals: [nodeExternals()],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
-        }
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', {
+          loader: "postcss-loader",
+          options: {
+            plugins: [
+              require('cssnano')({ preset: 'default' })
+            ],
+          },
+        }],
       },
+      { test: /\.(tsx|ts)$/, loader: "babel-loader", exclude: /node_modules/, },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
+        test: /\.(ico|jpg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "assets/[name].[hash:8].[ext]",
+        },
+      },
     ]
-  }
+  },
+  plugins: [new MiniCssExtractPlugin(), new ForkTsCheckerWebpackPlugin() ],
 };
