9c288952979bd66621698219393b063969c6467b
MrCrazyBelka <starpatrik06@gmail.com>
2020-03-24 20:37:27 +0300

    Add test

---
+++ src/components/Button/__tests__/Button.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '../Button';
+import { shallow } from 'enzyme';
+
+describe('Component [Button]', () => {
+  it('should render correctly', () => {
+    const component = shallow(<Button text="test" />);
+
+    expect(component).toHaveLength(1);
+  });
+});
